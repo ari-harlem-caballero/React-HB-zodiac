f684def7105ebc4cd31d29215b7ee5ff35f7d72a
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2022-01-24 15:28:14 -0800

    displayed zodiacs from main and ZodiacCard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
+import { zodiac } from './data'
+import Main from './Main/Main'
 {/* import the zodiac array */}
 
 function App() {
   return (
     <div className="App">
+      <Main zodiacs= {zodiac} />
       {/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
