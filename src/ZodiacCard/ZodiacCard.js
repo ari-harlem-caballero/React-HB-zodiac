c585ccc9ad4be9ab3965c4c5fcc05b5e314eb264
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2022-01-24 16:16:07 -0800

    added className to zodiaccard, confused where to add .name class/how to space

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,9 +1,9 @@
 import './ZodiacCard.css'
 
 export default function ZodiacCard(props) {
-  return <div>
+  return <div className='zodiac-card'>
     {props.name},
-    {props.dates},
+    {props.dates}
     <img src={`/images/${props.name}.png`} />
   </div>
 }
